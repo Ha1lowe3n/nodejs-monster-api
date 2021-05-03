bc45f77a522abc4b564643583aa6acc83ada7d78
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-05-03 21:56:26 +0800

    post, put, delete artist

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express()
 
-const artists = [
+// парсим правильно json
+app.use(bodyParser.json())
+// парсим правильно формы
+app.use(bodyParser.urlencoded({extended: true}))
+
+let artists = [
 	{id: 1, name: "John"},
 	{id: 2, name: "Vasya"},
 	{id: 3, name: "Olof"},
@@ -19,11 +25,28 @@ app.get("/artists", (req, res) => {
 app.get("/artists/:id", (req, res) => {
 	console.log(req.params.id)
 	const artist = artists.find(a => a.id === +req.params.id)
-	if (artist) {
-		res.send(artist)
-	} else {
-		res.send("artist not found")
+	artist ? res.send(artist) : res.send("artist not found")
+})
+
+app.post("/artists", (req,res) => {
+	const newArtist = {
+		id: Date.now(),
+		name: req.body.name
 	}
+	artists.push(newArtist)
+	res.send("post data")
+})
+
+app.put("/artists/:id", (req, res) => {
+	const artist = artists.find(a => a.id === +req.params.id)
+	artist.name = req.body.name
+	res.sendStatus(200)
+})
+
+app.delete("/artists/:id", (req,res) => {
+	artists = artists.filter(a => a.id !== +req.params.id)
+	res.sendStatus(200)
+	console.log(artists)
 })
 
 app.listen(3012, () => console.log("Server is running"))
